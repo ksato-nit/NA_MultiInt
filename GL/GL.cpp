efcefbfe3b80313787afd973d4ea20889aa9e103
Kaito Sato <ksato.nit@gmail.com>
2022-01-29 02:12:38 +0900

    [add] J_k^2 の計算を実装

--- GL/GL.cpp
+++ GL/GL.cpp
@@ -6,6 +6,7 @@ using namespace std;
 double zeroOfBessel(int);
 
 int main(void){
+    cout << Jsquared(1) << endl;
     return 0;
 }
 
@@ -17,4 +18,13 @@ double zeroOfBessel(int k){
 
     double j = a + 1 / b - 124 / (3 * pow(b, 3.0)) + 120928 / (15 * pow(b, 5)) - 401743168 / (105 * pow(b, 7));
     return j;
+}
+
+/* zeroOfBessel(k) における 1 次 Bessel 関数の 2 乗を返す． */
+double Jsquared(int k){
+    double pi = boost::math::constants::pi<double>();
+    double a = pi * (k - 0.25);
+    
+    double J = (2 - (7 / (24 * pow(a, 4))) + (151 / (80 * pow(a, 6))) - (172913 / (8064 * pow(a, 8)))) / (pi * a);
+    return J;
 }
