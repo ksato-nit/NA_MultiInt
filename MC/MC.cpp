761b7d82903441de8269abaaa8c05642bec0c3f1
Kaito Sato <ksato.nit@gmail.com>
2022-01-29 07:52:21 +0900

    [fix] 乱数生成法を変更

--- MC/MC.cpp
+++ MC/MC.cpp
@@ -10,15 +10,15 @@ int main(void){
     double S, x[D] = {0};
 
     random_device r;
-    mt19937 mt(r());
+    minstd_rand mr(r());
     uniform_real_distribution<> ud(-1, 1);
 
     for(int n = 10; n < 10000; ++n){
         S = 0;
+        x[1] = ud(mr);
         for(int i = 1; i <= n; ++i){
-            for(int d = 0; d < D; ++d){
-                x[d] = ud(mt);
-            }
+            x[0] = x[1];
+            x[1] = ud(mr);
             S += func(x);
         }
         S = (pow(2, D) * S) / n;
