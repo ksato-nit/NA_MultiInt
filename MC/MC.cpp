6ffa3922348873a6297f50ec7e0c039ce075c510
Kaito Sato <ksato.nit@gmail.com>
2022-01-30 19:40:51 +0900

    [fix] データ収集用にコードを変更

--- MC/MC.cpp
+++ MC/MC.cpp
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
-#define D 2
+#define D 4
 
 using namespace std;
 
 double func(double*);
+double lcg();
 
 int main(void){
     ofstream outputfile("result.dat");
     double S, x[D] = {0};
 
     random_device r;
+    mt19937 mt(r());
     minstd_rand mr(r());
     uniform_real_distribution<> ud(-1, 1);
 
-    for(int n = 1; n < 1000; ++n){
-        S = 0;
-        x[1] = ud(mr);
-        for(int i = 1; i <= 10000; ++i){
-            x[0] = x[1];
-            x[1] = ud(mr);
-            S += func(x);
+    for(int m = 1; m <= 25; ++m){
+        int n = (int) pow(2, m);
+        double avg = 0;
+        for(int k = 0; k < 100; ++k){
+            S = 0;
+            for(int i = 1; i <= n; ++i){
+                for(int j = 0; j < D; ++j){
+                    x[j] = lcg();
+                }
+                S += func(x);
+            }
+            S = (pow(2, D) * S) / n;
+            avg += S;
         }
-        S = (pow(2, D) * S) / 10000;
-        outputfile << setprecision(30) << abs(S - pow(exp(1) - exp(-1), D)) / (pow(exp(1) - exp(-1), D)) << endl;
+        avg = avg / 100;
+        outputfile << setprecision(30) << abs(avg - pow(exp(1) - exp(-1), D)) / (pow(exp(1) - exp(-1), D)) << endl;
     }
     outputfile.close();
     return 0;
@@ -35,4 +43,13 @@ double func(double* x){
         e += -x[d];
     }
     return exp(e);
+}
+
+double lcg(void){
+    static long long int x = 15;
+    long long int m = (long long int) pow(2, 31);
+    int a = 65539, c = 0;
+    x = (a * x + c) % m;
+
+    return (2.0 * x / m) - 1.0;
 }
