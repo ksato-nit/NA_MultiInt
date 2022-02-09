95bbe8d597bee8fd811ecdb1d03ef0c5c33b52c2
Kaito Sato <ksato.nit@gmail.com>
2022-02-10 02:08:18 +0900

    [add] "ストラックアウト"分割による MC 法の実装

--- MC/MC.cpp
+++ MC/MC.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define D 4
+#define D 2
 
 using namespace std;
 
@@ -14,8 +14,9 @@ int main(void){
     mt19937 mt(r());
     minstd_rand mr(r());
     uniform_real_distribution<> ud(-1, 1);
+    double truth = pow(M_PI / 2, D);
 
-    for(int m = 1; m <= 25; ++m){
+    for(int m = 1; m <= 20; ++m){
         int n = (int) pow(2, m);
         double avg = 0;
         for(int k = 0; k < 100; ++k){
@@ -30,7 +31,7 @@ int main(void){
             avg += S;
         }
         avg = avg / 100;
-        outputfile << setprecision(30) << abs(avg - pow(exp(1) - exp(-1), D)) / (pow(exp(1) - exp(-1), D)) << endl;
+        outputfile << setprecision(30) << abs(avg - truth) / truth << endl;
     }
     outputfile.close();
     return 0;
@@ -38,17 +39,17 @@ int main(void){
 
 /* 被積分関数．*/
 double func(double* x){
-    double e = 0;
+    double S = 1;
     for(int d = 0; d < D; ++d){
-        e += -x[d];
+        S *= 1 / (1 + pow(x[d], 2));
     }
-    return exp(e);
+    return S;
 }
 
 double lcg(void){
     static long long int x = 15;
     long long int m = (long long int) pow(2, 31);
-    int a = 65539, c = 0;
+    int a = 1103515245, c = 12345;
     x = (a * x + c) % m;
 
     return (2.0 * x / m) - 1.0;
--- MC/MC_density.cpp
+++ MC/MC_density.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#define D 4
+#define D 2
 
 using namespace std;
 
@@ -16,18 +16,65 @@ int main(void){
     minstd_rand mr(r());
     uniform_real_distribution<> ud(-1, 1);
 
-    for(int m = 1; m <= 15; ++m){
+    for(int m = 4; m <= 19; ++m){
         int n = (int) pow(2, m);
         double avg = 0;
         for(int k = 0; k < 100; ++k){
             S = 0;
-            for(int i = 1; i <= n; ++i){
-                for(int j = 0; j < D; ++j){
-                    x[j] = lcg(-1.0, 1.0);
-                }
-                S += func(x);
+            double S1 = 0, S2 = 0, S3 = 0;
+
+            /* 分点数を大きく 8 分割し，領域 1, 3, 7, 9 に 2, 領域 2, 4, 6, 8 に 4，領域 5 に 2 を割り当てる．*/
+            for(int i = 1; i <= n / 16; ++i){
+                x[0] = lcg(-1.0, -1.0 / 3);
+                x[1] = lcg(1.0 / 3, 1.0);
+                S1 += func(x);
             }
-            S = (pow(2, D) * S) / n;
+            for(int i = 1; i <= n / 16; ++i){
+                x[0] = lcg(1.0 / 3, 1.0);
+                x[1] = lcg(1.0 / 3, 1.0);
+                S1 += func(x);
+            }
+            for(int i = 1; i <= n / 16; ++i){
+                x[0] = lcg(-1.0, -1.0 / 3);
+                x[1] = lcg(-1.0, -1.0 / 3);
+                S1 += func(x);
+            }
+            for(int i = 1; i <= n / 16; ++i){
+                x[0] = lcg(1.0 / 3, 1.0);
+                x[1] = lcg(-1.0, -1.0 / 3);
+                S1 += func(x);
+            }
+            S += (4 * (4.0 / 9) * S1) / (n / 4);
+
+            for(int i = 1; i <= n / 8; ++i){
+                x[0] = lcg(-1.0 / 3, 1.0 / 3);
+                x[1] = lcg(1.0 / 3, 1.0);
+                S2 += func(x);
+            }
+            for(int i = 1; i <= n / 8; ++i){
+                x[0] = lcg(-1.0, -1.0 / 3);
+                x[1] = lcg(-1.0 / 3, 1.0 / 3);
+                S2 += func(x);
+            }
+            for(int i = 1; i <= n / 8; ++i){
+                x[0] = lcg(1.0 / 3, 1.0);
+                x[1] = lcg(-1.0 / 3, 1.0 / 3);
+                S2 += func(x);
+            }
+            for(int i = 1; i <= n / 8; ++i){
+                x[0] = lcg(-1.0 / 3, 1.0 / 3);
+                x[1] = lcg(-1.0, -1.0 / 3);
+                S2 += func(x);
+            }
+            S += (4 * (4.0 / 9) * S2) / (n / 2);
+
+            for(int i = 1; i <= (n / 4) + n; ++i){
+                x[0] = lcg(-1.0 / 3, 1.0 / 3);
+                x[1] = lcg(-1.0 / 3, 1.0 / 3);
+                S3 += func(x);
+            }
+
+            S += ((4.0 / 9) * S3) / ((n / 4) + n);
             avg += S;
         }
         avg = avg / 100;
@@ -48,6 +95,9 @@ double func(double* x){
 
 /* [b1, b2] 内の一様乱数を線形合同法により生成する．*/
 double lcg(double b1, double b2){
+    if(b1 > b2){
+        swap(b1, b2);
+    }
     static long long int x = 15;
     long long int m = (long long int) pow(2, 31);
     int a = 1103515245, c = 12345;
